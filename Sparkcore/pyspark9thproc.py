fffddb1de2d72e5acbc859adb6db9c1e5087dc91
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-08-17 12:18:47 +0530

    v_0.0.1

--- Sparkcore/pyspark9thproc.py
+++ Sparkcore/pyspark9thproc.py
@@ -6,6 +6,7 @@ spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 sc = spark.sparkContext
 data="C:\\Users\\chinm\\Downloads\\GMT20220815-054205_Recording.txt"
 rdd=sc.textFile(data)
-res=rdd.filter(lambda x:"age" not in x).map(lambda x:x.split(' ')).filter(lambda x:"@" in x).map(lambda x:(x[0],x[1]))
+res=rdd.map(lambda x:x.split('\t')).map(lambda x:(x[0],x[1]))
+
 for i in res.collect():
     print(i)
