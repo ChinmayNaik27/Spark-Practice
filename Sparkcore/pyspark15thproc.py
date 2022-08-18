9b885efc523b9415ebc4a71e6f477f37fb343606
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-08-18 13:31:14 +0530

    v_0.0.1

--- Sparkcore/pyspark15thproc.py
+++ Sparkcore/pyspark15thproc.py
@@ -7,7 +7,7 @@ sc = spark.sparkContext
 
 rdd=sc.textFile("D:/Big-Data-Files_&_notes/Spark/drivers/asl.csv")
 
-res=rdd.map(lambda x:x.split(',')).filter(lambda x:"age" not in x).map(lambda x:x[3]).sortBy(lambda x:x[3])
+res=rdd.map(lambda x:x.split(',')).filter(lambda x:"age" not in x).map(lambda x:x[2]).sortBy(lambda x:x[2])
 
 for i in res.collect():
     print(i)
