b22f517fe75491d81bc79610147e738b80f30067
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-08-21 17:35:57 +0530

    Spark-programs

--- Sparkcore/pyspark33rdproc.py
+++ Sparkcore/pyspark33rdproc.py
@@ -6,5 +6,5 @@ spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 sc = spark.sparkContext
 data="C:\\Users\\chinm\\OneDrive\\Documents\\record1.csv"
 df=spark.read.format('csv').option('header','true').option('inferSchema','true').load(data)
-ndf=df.withColumn("newcol",regexp_replace(col('order id'),"-",'')).drop("country",'city','state')
+ndf=df.withColumn("newcol",regexp_replace(col('order id'),"-",'')).drop("country",'city','state','sales')
 ndf.show()
