d4f9153c91abdfcf4a12a2a14dac05739d8223a0
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-08-21 16:02:05 +0530

    Spark_programs

--- Sparkcore/pyspark27thproc.py
+++ Sparkcore/pyspark27thproc.py
@@ -1,10 +1,12 @@
-#importing data from a csv file  and adding a new column
+#importing data from a csv file  and adding a new column with defalut value of one
 from pyspark.sql import *
 from pyspark.sql.functions import *
 
 spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 sc = spark.sparkContext
 
-df=spark.read.format('csv').option('header','true').option('inferSchama','true').load("C:\\Users\\chinm\\OneDrive\\Documents\\record1.csv")
-#if column deos not exists then
-df.show()
+df=spark.read.format('csv').option('header','true').option('inferSchema','true').load("C:\\Users\\chinm\\OneDrive\\Documents\\record1.csv")
+#if column deos not exists then update or else add
+ndf=df.withColumn('Quantitynew',lit(1))
+ndf.show()
+ndf.printSchema()
