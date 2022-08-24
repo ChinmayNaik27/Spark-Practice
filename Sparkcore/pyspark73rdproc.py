018b2a9ec162ffcde4ebf4d3004658ea5e00fae8
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-08-24 22:37:57 +0530

    spark-programs

--- Sparkcore/pyspark73rdproc.py
+++ Sparkcore/pyspark73rdproc.py
@@ -6,5 +6,12 @@ spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 sc = spark.sparkContext
 win=Window.partitionBy('state').orderBy(col('zip').desc())
 df=spark.read.format('csv').option('header','true').option('inferSchema','true').load("C:/bigdata/DATASETS/us-500.csv")
-ndf=df.withColumn('lead',ntile(4))
+ndf=df.withColumn('ntile _column',ntile(5).over(win))
 ndf.show()
+ndf1=df.withColumn('ntile_col',ntile(10).over(win)).where(col('ntile_col')==1)
+ndf1.show()
+
+#sql friendly
+df.createOrReplaceTempView('tab')
+ndf2=spark.sql("select * from tab where zip between 85260 and 85381")
+ndf2.show()
