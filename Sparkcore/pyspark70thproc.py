7b44114d1f0c1a6815121b17a35ebf703f611aed
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-08-24 21:57:36 +0530

    spark-programs

--- Sparkcore/pyspark70thproc.py
+++ Sparkcore/pyspark70thproc.py
@@ -6,6 +6,6 @@ spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 sc = spark.sparkContext
 df=spark.read.format('csv').option('header','true').option('inferSchema','true').load("C:/bigdata/DATASETS/us-500.csv")
 df.show()
-win=Window.partitionBy('state').orderBy(col('first_name'))
+win=Window.partitionBy('state').orderBy(col('zip'))
 ndf=df.withColumn('rnk',rank().over(win))
 ndf.show()
--- Sparkcore/pyspark71thproc.py
+++ Sparkcore/pyspark71thproc.py
@@ -4,5 +4,7 @@ from pyspark.sql.functions import *
 from pyspark.sql import Window
 spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 sc = spark.sparkContext
-win=Window.partitionBy('state').orderBy(col('city').desc())
-df=
+win=Window.partitionBy('state').orderBy(col('zip').desc())
+df=spark.read.format('csv').option('header','true').option('inferSchema','true').load("C:/bigdata/DATASETS/us-500.csv")
+ndf=df.withColumn('drnk',dense_rank().over(win))
+ndf.show()
