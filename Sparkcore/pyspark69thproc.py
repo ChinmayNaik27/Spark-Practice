04ccf208697678c5525cd92a0b5c8023e52b5cca
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-08-24 21:30:26 +0530

    spark-programs

--- Sparkcore/pyspark69thproc.py
+++ Sparkcore/pyspark69thproc.py
@@ -5,7 +5,7 @@ from pyspark.sql.functions import *
 from pyspark.sql.window import Window
 spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 sc = spark.sparkContext
-win=Window.orderBy(col('sal'))
+win=Window.orderBy(col('first_name'))
 df=spark.read.csv("C:/bigdata/DATASETS/us-500.csv",header=True,inferSchema=True)
 ndf=df.withColumn('rownum',row_number().over(win))
 ndf.show()
