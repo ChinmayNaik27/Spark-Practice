5d37d5b328c78e4267ae8e22fae98babb990bdea
Chinmay Naik <94610060+ChinmayNaik27@users.noreply.github.com>
2022-08-24 09:14:44 +0530

    spark-program

--- Sparkcore/pyspark21stproc.py
+++ Sparkcore/pyspark21stproc.py
@@ -6,7 +6,7 @@ sc = spark.sparkContext
 data="C:/Users/chinm/Downloads/bank-full - Copy.csv"
 rdd=sc.textFile(data)
 skip=rdd.first()
-odata=rdd.filter(lambda x:x!=skip)
+odata=rdd.map(lambda x:x.replace("\"","")).filter(lambda x:x!=skip)
 res=spark.read.csv(odata,header=True,inferSchema=True,sep=";")
 # res=spark.read.format('csv').option('header','true').option('inferSchema','true').option('sep',';').load(odata)
 res.show()
